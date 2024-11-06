8f667d59d7dea578db55aa5c6a9e5ddd567bb414
igorkrosh <igor.krosh@gmail.com>
2024-11-06 14:11:34 +0300

    Поддержка .pug файлов

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const PugPlugin = require('pug-plugin');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, 'dist/'),
+        clean: true
+    },
+    plugins: [
+        new PugPlugin({
+            entry: 'src/pages/',
+        })
+    ],
+    mode: 'production',
+    resolve: {
+        alias: {
+            '@src': path.resolve(__dirname, 'src/'),
+        }
+    }
+}
