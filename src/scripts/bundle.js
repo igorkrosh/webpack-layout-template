a021efd0abda396ec8ce9d0c2c899f005376ca87
igorkrosh <igor.krosh@gmail.com>
2024-12-09 12:50:28 +0300

    Реализация табов

--- src/scripts/bundle.js
+++ src/scripts/bundle.js
@@ -13,7 +13,8 @@ function Core()
     InitLightbox();
     InitMaskPlugin();
 
-    SetAnchor()
+    SetAnchor();
+    SetTabSwitcher();
 }
 
 function InitLightbox()
@@ -32,5 +33,42 @@ function SetAnchor()
         e.preventDefault();
         let anchor = $(this).attr('anchor');
         $(anchor)[0].scrollIntoView({ behavior: "smooth" });
+    })
+}
+
+function SetTabSwitcher()
+{
+    $('.btn-tab').on('click', function(e) {
+        e.preventDefault();
+
+        if ($(this).hasClass('active'))
+            return;
+
+        $('.btn-tab').removeClass('active');
+        $(this).addClass('active');
+
+        let tabHeight = $(`.tab.active`)[0].clientHeight;
+        $(`.tab.active`).closest('.tab-viewer').css('height', `${tabHeight}px`)
+
+        SwitchTab($(this).attr('target'))
+    })
+}
+
+function SwitchTab(target)
+{
+    $('.tab.active').animate({
+        opacity: 0
+    }, 500, function() {
+        $('.tab.active').removeClass('active');
+
+        $(`[tab-name="${target}"]`).css('opacity', 0);
+        $(`[tab-name="${target}"]`).addClass('active');
+
+        let tabHeight = $(`[tab-name="${target}"]`)[0].clientHeight;
+        $(`[tab-name="${target}"]`).closest('.tab-viewer').css('height', `${tabHeight}px`)
+
+        $(`[tab-name="${target}"]`).animate({
+            opacity: 1
+        }, 500)
     })
 }
